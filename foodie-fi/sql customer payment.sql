b950d5983d09dea320d97a0dc3433787e4f056af
Sowjanya Chilaka <104596844+swjnch@users.noreply.github.com>
2022-06-26 13:58:55 -0400

    Update sql customer payment.sql

--- foodie-fi/sql customer payment.sql
+++ foodie-fi/sql customer payment.sql
@@ -1,25 +1,20 @@
 USE foodie_fi;
 
-with ct as(
-SELECT *,
-        RANK () OVER (partition by customer_id order by plan_id) AS ranking,
-        (CASE 
-           WHEN plan_id IN (1,2,3) AND 
-				(RANK () OVER (partition by customer_id order by plan_id))=1 THEN 1 END) payment_order,
-        LEAD(plan_name,1) OVER (PARTITION BY customer_id ORDER BY start_date) nextplan,
-        LEAD(start_date,1) OVER (PARTITION BY customer_id ORDER BY start_date) enddate
-FROM 
-subscriptions
-JOIN
-plans
-USING(plan_id)
-WHERE year(start_date) = 2020 AND plan_id <> 0)
-SELECT *,
-         CASE
-          WHEN plan_id = 3 AND nextplan IS NULL THEN start_date
-          WHEN plan_id = 4 THEN NULL
-          WHEN enddate IS NOT NULL THEN enddate ELSE '2020-12-31' END AS payment_date
-FROM ct;
+SELECT customer_id,
+       plan_id,
+       plan_name,
+       start_date,
+       price,
+	   (CASE 
+             WHEN plan_id = 4 THEN NULL
+             WHEN plan_id = 3 THEN start_date
+             WHEN LEAD(plan_name,1) OVER (PARTITION BY customer_id ORDER BY start_date) IS NOT NULL THEN 
+             LEAD(start_date,1) OVER (PARTITION BY customer_id ORDER BY start_date) 
+             ELSE '2020-12-31' END) AS payment_date,
+       RANK() OVER(PARTITION BY customer_id ORDER BY start_date) AS payment_order
+FROM foodie_fi.subscriptions
+JOIN foodie_fi.plans USING(plan_id)
+WHERE date_part('year',start_date) = 2020 AND plan_id <> 0;
 
 
 
