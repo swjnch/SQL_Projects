d5b84e58f2c4ce2bc5faaac88e9b0cb0f1eea8d3
Sowjanya Chilaka <104596844+swjnch@users.noreply.github.com>
2022-08-09 11:59:47 -0400

    Add files via upload

--- Data Mart/DataExploration.sql
+++ Data Mart/DataExploration.sql
@@ -52,7 +52,7 @@ SELECT platform,
 FROM clean_weekly_sales AS ws1
 GROUP BY platform, calendar_year, month_number
 ORDER BY platform, calendar_year, month_number)
-SELECT 
+SELECT platform,
        ps.calendar_year,
        TO_CHAR(TO_DATE(ps.month_number::text,'MM'),'Mon') AS Month_name,
        ROUND(platform_sales * 100/total_sales::NUMERIC,2)  AS perc_sales
@@ -62,24 +62,59 @@ ON sc.calendar_year=ps.calendar_year
 AND sc.month_number=ps.month_number;
 
 -- What is the percentage of sales by demographic for each year in the dataset?
-WITH demo_sales_cte AS(
+WITH sales_cte AS(
+SELECT calendar_year,
+       SUM(sales) AS yearly_sales
+FROM clean_weekly_sales AS ws
+GROUP BY calendar_year
+ORDER BY calendar_year),
+demographic_sales AS(
 SELECT demographic,
-       SUM(sales) AS total_sales
-FROM clean_weekly_sales
-GROUP BY demographic)
+       calendar_year,                                                           
+       SUM(sales) AS demographic_sales
+FROM clean_weekly_sales 
+GROUP BY demographic, calendar_year
+ORDER BY demographic, calendar_year)
 SELECT demographic,
-       calendar_year,
-       SUM(sales) * 100/(SELECT total_sales FROM demo_sales_cte)AS yearly_sales
-FROM clean_weekly_sales
-GROUP BY demographic, calendar_ye
-ORDER BY demographic, calendar_year;
-
-
-
-
+       ds.calendar_year,
+       ROUND(demographic_sales * 100/yearly_sales::NUMERIC,1)  AS perc_sales
+FROM sales_cte sc
+JOIN demographic_sales ds
+ON sc.calendar_year=ds.calendar_year;
 
 -- Which age_band and demographic values contribute the most to Retail sales?
--- Demographic and age_band
+-- Demographic
+SELECT demographic,
+       SUM(sales) as total_sales,
+       ROUND(100 * SUM(sales) / SUM(SUM(sales)) OVER(), 2) AS perc_sales
+FROM clean_weekly_sales
+WHERE platform = 'Retail'
+GROUP BY demographic
+ORDER BY demographic;
 
+-- Age band
+SELECT age_band,
+       SUM(sales) as total_sales,
+       ROUND(100 * SUM(sales) / SUM(SUM(sales)) OVER(), 2) AS perc_sales
+FROM clean_weekly_sales
+WHERE platform = 'Retail'
+GROUP BY age_band
+ORDER BY age_band;
+
+-- Demographic and Age_band
+SELECT demographic,
+       age_band,
+       SUM(sales) as total_sales,
+       SUM(SUM(sales)) OVER(),
+       ROUND(100 * SUM(sales)/SUM(SUM(sales)) OVER(), 2) AS perc_sales
+FROM clean_weekly_sales
+WHERE platform = 'Retail'
+GROUP BY demographic, age_band
+ORDER BY demographic, age_band;
 
 -- Can we use the avg_transaction column to find the average transaction size for each year for Retail vs Shopify? If not - how would you calculate it instead?
+SELECT platform,
+       calendar_year,
+       ROUND(SUM(sales)/SUM(transactions)::NUMERIC,2) AS avg_transactions
+FROM clean_weekly_sales
+GROUP BY platform, calendar_year;
